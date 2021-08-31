11031c49d22ee5a4708eb82b3bcd7572b9cf4795
Dilum Aluthge <dilum@aluthge.com>
2021-08-30 22:53:00 -0400

    Improve the `Check README` CI check (#12)

--- .ci/readme.jl
+++ .ci/readme.jl
@@ -1,15 +1,16 @@
 using Test
 
-function check_readme()
-    repository_root = dirname(@__DIR__)
+function check_readme(repository_root::AbstractString)
     readme_path = joinpath(repository_root, "README.md")
     example_path = joinpath(repository_root, "example.yml")
-    readme_contents = read(readme_path, String)::String
-    example_contents = read(example_path, String)::String
+    readme_contents = read(readme_path, String)
+    example_contents = string(
+        "```yaml\n",
+        read(example_path, String),
+        "```\n",
+    )
     @testset "Check README" begin
         @test occursin(example_contents, readme_contents)
     end
     return nothing
 end
-
-check_readme()
